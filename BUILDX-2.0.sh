4e82492d242bbd33e1656548aafe9cd61cab1e8a
Thorsten Ludewig <t.ludewig@ostfalia.de>
2021-10-19 12:47:59 +0200

    build 1.4 versions also

---
+++ BUILDX-2.0.sh
@@ -0,0 +1,1 @@
+./BUILDX-latest.sh 2.0.10 2.0
--- BUILDX-latest.sh
+++ BUILDX-latest.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-if [ $# -ge 1 ];
 BUILD_VERSION=$1
 echo "BUILD_VERSION=$BUILD_VERSION"
 docker buildx build --build-arg BUILD_VERSION="$BUILD_VERSION" \
 --push \
 --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
 --tag "tludewig/389ds:$BUILD_VERSION-alpine" .
-fi
 
-if [ $# -ge 2 ];
 docker buildx build --build-arg BUILD_VERSION="$BUILD_VERSION" \
 --push \
 --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
 --tag "tludewig/389ds:$2-alpine" .
-fi
 
-if [ $# -ge 3 ];
 docker buildx build --build-arg BUILD_VERSION="$BUILD_VERSION" \
 --push \
 --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
---tag "tludewig/389ds:$3-alpine" .
-fi
+--tag "tludewig/389ds" .
