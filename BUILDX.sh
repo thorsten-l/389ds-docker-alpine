491a23845771ce254c49b5b64e351b885533c014
Thorsten Ludewig <t.ludewig@ostfalia.de>
2021-10-19 12:49:56 +0200

    keyword then was missing

--- BUILDX.sh
+++ BUILDX.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $# -ge 1 ];
+if [ $# -ge 1 ]; then
 BUILD_VERSION=$1
 echo "BUILD_VERSION=$BUILD_VERSION"
 docker buildx build --build-arg BUILD_VERSION="$BUILD_VERSION" \
@@ -9,14 +9,14 @@ docker buildx build --build-arg BUILD_VERSION="$BUILD_VERSION" \
 --tag "tludewig/389ds:$BUILD_VERSION-alpine" .
 fi
 
-if [ $# -ge 2 ];
+if [ $# -ge 2 ]; then
 docker buildx build --build-arg BUILD_VERSION="$BUILD_VERSION" \
 --push \
 --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
 --tag "tludewig/389ds:$2-alpine" .
 fi
 
-if [ $# -ge 3 ];
+if [ $# -ge 3 ]; then
 docker buildx build --build-arg BUILD_VERSION="$BUILD_VERSION" \
 --push \
 --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
